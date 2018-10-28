7f6917d3362734c2738229542dc62b4f3907b130
Jesus Jimenez <jedar93@gmail.com>
2018-10-27 21:03:33 -0400

    added db config stuff

--- config/env/production.js
+++ config/env/production.js
@@ -1,11 +1,13 @@
 'use strict';
 
 exports.db = {
-    uri: process.env.MONGODB_URI,
-    user: process.env.MONGODB_USER,
-    password: process.env.MONGODB_PASSWORD
+    uri: process.env.MONGODB_URI || '',
+    user: process.env.MONGODB_USER || '',
+    password: process.env.MONGODB_PASSWORD || '',
+    user_id: '5bc96ce4f5c7438c03959491',
+    admin_id: '5bc96aa9f5c7438c0395948f'
 };
 
 exports.iron = {
-    password: process.env.IRON_PASSWORD
+    password: process.env.IRON_PASSWORD || ''
 };
